1ea98d84fc6d0cd4225a0dddae88aa00f9f3ae7d
Renddi Yeh <renddi@relab.cc>
2021-07-27 00:30:18 +0800

    Updates

---
+++ component---src-pages-404-js-b3b5aefa78ab5c88e2b8.js
@@ -0,0 +1,2 @@
+(self.__LOADABLE_LOADED_CHUNKS__=self.__LOADABLE_LOADED_CHUNKS__||[]).push([[883],{9616:function(e,t,l){"use strict";l.r(t);var u=l(5007),n=l(9350);t.default=function(){return u.default.createElement(n.Z,null,u.default.createElement("h1",null,"NOT FOUND"),u.default.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b3b5aefa78ab5c88e2b8.js.map
