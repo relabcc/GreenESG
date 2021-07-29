786e381c262e28ac9d5e3d798b080a77472cf052
Peja <peja@relab.cc>
2021-07-29 19:21:12 +0800

    Updates

---
+++ d64684d8-8e10e9db54c983fab511.js
@@ -0,0 +1,2 @@
+(self.__LOADABLE_LOADED_CHUNKS__=self.__LOADABLE_LOADED_CHUNKS__||[]).push([[556],{6370:function(t,r,n){"use strict";n.d(r,{A4_:function(){return o}});var e=n(1046);function o(t){return(0,e.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M19 13l-7 7-7-7m14-8l-7 7-7-7"}}]})(t)}}}]);
+//# sourceMappingURL=d64684d8-8e10e9db54c983fab511.js.map
