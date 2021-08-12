b3061696c82c439417b05eb0849173fa0885c740
Renddi Yeh <renddi@relab.cc>
2021-08-12 18:23:39 +0800

    Updates

---
+++ 95b64a6e-af11ea0860b7deb18eb2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstyled_gatsby_starter=self.webpackChunkstyled_gatsby_starter||[]).push([[866],{1424:function(t,s,a){a.d(s,{Vmf:function(){return r}});var e=a(1046);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M256 217.9L383 345c9.4 9.4 24.6 9.4 33.9 0 9.4-9.4 9.3-24.6 0-34L273 167c-9.1-9.1-23.7-9.3-33.1-.7L95 310.9c-4.7 4.7-7 10.9-7 17s2.3 12.3 7 17c9.4 9.4 24.6 9.4 33.9 0l127.1-127z"}}]})(t)}}}]);
+//# sourceMappingURL=95b64a6e-af11ea0860b7deb18eb2.js.map
